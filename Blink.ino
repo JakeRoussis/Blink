c63ad3d53ef9003b3ea916b96cc10185555d44a7
JakeRoussis <40971419+JakeRoussis@users.noreply.github.com>
2018-07-09 14:25:15 +1000

    Upload Blink.io
    Source code for Arduino blink

---
+++ Blink.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn LED on
+  delay(1000);                       // wait 1 second
+  digitalWrite(LED_BUILTIN, LOW);    // turn LED off
+  delay(1000);                       // wait 1 second
+}
